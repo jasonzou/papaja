f8702f63e821f1bb6c9eff3a512a942fdf1a3a77
mariusbarth <marius.barth@uni-koeln.de>
2015-06-18 18:33:30 +0200

    Fixed a bug in apa_barplot(). Now ylim is chosen more wisely for negative data.

--- R/apa_barplot.R
+++ R/apa_barplot.R
@@ -145,7 +145,7 @@ apa_barplot <- function(
 
   ## Adjust ylim to hight of error bars
   if(is.null(ellipsis$ylim)) {
-    ellipsis$ylim <- c(min(0, yy[, dv] + ee[, dv]), max(yy[, dv] - ee[, dv]))
+    ellipsis$ylim <- c(min(0, yy[, dv] + ee[, dv]), max(0, yy[, dv] - ee[, dv]))
   }
 
 
